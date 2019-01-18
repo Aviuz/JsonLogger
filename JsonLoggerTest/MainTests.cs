39b6bb5a899f86dbcd7a692404bc6421a389c008
Avius <avius.ravis@gmail.com>
2019-01-18 18:15:05 +0100

    Init

---
+++ JsonLoggerTest/MainTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonLoggerTest
+{
+    [TestClass]
+    public class MainTests
+    {
+        [TestMethod]
+        public void InitializeFile()
+        {
+            string path = "C:/tmp/empty.txt";
+
+            File.Delete(path);
+
+            var logger = new JsonLogger.JsonLogger(path);
+            logger.SaveChanges();
+
+            string contentOfFile = File.ReadAllText(path);
+            Assert.AreEqual("[\r\n]", contentOfFile);
+        }
+
+        [TestMethod]
+        public void Log()
+        {
+            string path = "C:/tmp/log.txt";
+
+            var logger = new JsonLogger.JsonLogger(path);
+
+            logger.LogMessage("test");
+
+            logger.SaveChanges();
+
+            string contentOfFile = File.ReadAllText(path);
+            Assert.AreEqual("[\r\n]", contentOfFile);
+        }
+    }
+}
