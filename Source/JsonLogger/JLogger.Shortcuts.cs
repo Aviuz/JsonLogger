4353af892133b19f23acee7cefb8bef4a02ee6f2
Avius <avius.ravis@gmail.com>
2020-11-14 12:35:11 +0100

    chnaged shortcuts from extensions to partial class

---
+++ Source/JsonLogger/JLogger.Shortcuts.cs
@@ -0,0 +1,70 @@
+﻿using JsonLogger.Enums;
+using JsonLogger.Formatting;
+using JsonLogger.Sinks;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace JsonLogger
+{
+    partial class JLogger
+    {
+        /// <summary>
+        /// Logs single-line text with specified level
+        /// </summary>
+        public void Log(string text, LogLevel logLevel = LogLevel.Info)
+            => LogText(text, null, logLevel);
+
+        /// <inheritdoc cref="JLogger.LogText(string, string, LogLevel)" />
+        public void Log(string title, string description, LogLevel logLevel = LogLevel.Info)
+            => LogText(title, description, logLevel);
+
+        /// <summary>
+        /// Logs exception with it's properties as JSON object with default 'Critical' level
+        /// </summary>
+        public void Log(string title, Exception exception, LogLevel logLevel = LogLevel.Critical)
+            => LogException(title, exception, logLevel);
+
+        /// <summary>
+        /// Logs exception with it's properties as JSON object with default 'Critical' level
+        /// </summary>
+        public void Log(Exception exception, LogLevel logLevel = LogLevel.Critical)
+            => LogException(null, exception, logLevel);
+
+        /// <summary>
+        /// Logs objects with it's public properties and fields as JSON object
+        /// </summary>
+        /// <param name="title">Title for entry</param>
+        /// <param name="item">Object that will be logged</param>
+        /// <param name="logLevel">Level for entry</param>
+        public void Log(string title, object item, LogLevel logLevel = LogLevel.Info)
+            => LogObject(title, item, logLevel);
+
+        /// <summary>
+        /// Logs objects with it's public properties and fields as JSON object
+        /// </summary>
+        /// <param name="item">Object that will be logged</param>
+        /// <param name="logLevel">Category for entry</param>
+        public void Log(object item, LogLevel logLevel = LogLevel.Info)
+            => LogObject(null, item, logLevel);
+
+        /// <summary>
+        /// Logs single-line text with 'Warning' level
+        /// </summary>
+        public void Warning(string text)
+            => LogText(text, null, LogLevel.Warning);
+
+        /// <summary>
+        /// Logs single-line text with 'Warning' level
+        /// </summary>
+        public void Error(string text)
+            => LogText(text, null, LogLevel.Critical);
+
+        /// <inheritdoc cref="JLogger.LogException(string, Exception, LogLevel)" />
+        public void Exception(string title, Exception exception, LogLevel logLevel = LogLevel.Info)
+            => LogException(title, exception, logLevel);
+
+        /// <inheritdoc cref="JLogger.LogObject(string, object, LogLevel)" />
+        public void Object(string title, object data, LogLevel logLevel = LogLevel.Info)
+            => LogObject(title, data, logLevel);
+    }
+}
--- Source/JsonLogger/JLogger.cs
+++ Source/JsonLogger/JLogger.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 
 namespace JsonLogger
 {
-    public class JLogger
+    public partial class JLogger
     {
         private ISink sink;
         private readonly EntryFormatter entryFormatter;
@@ -78,67 +78,5 @@ namespace JsonLogger
         {
             LogText("Failed to log entry", $"StackTrace: {ex.StackTrace}", LogLevel.Critical);
         }
-    }
-
-    public static class JLoggerShortcuts
-    {
-        /// <summary>
-        /// Logs single-line text with specified level
-        /// </summary>
-        public static void Log(this JLogger logger, string text, LogLevel logLevel = LogLevel.Info)
-            => logger.LogText(text, null, logLevel);
-
-        /// <inheritdoc cref="JLogger.LogText(string, string, LogLevel)" />
-        public static void Log(this JLogger logger, string title, string description, LogLevel logLevel = LogLevel.Info)
-            => logger.LogText(title, description, logLevel);
-
-        /// <summary>
-        /// Logs exception with it's properties as JSON object with default 'Critical' level
-        /// </summary>
-        public static void Log(this JLogger logger, string title, Exception exception, LogLevel logLevel = LogLevel.Critical)
-            => logger.LogException(title, exception, logLevel);
-
-        /// <summary>
-        /// Logs exception with it's properties as JSON object with default 'Critical' level
-        /// </summary>
-        public static void Log(this JLogger logger, Exception exception, LogLevel logLevel = LogLevel.Critical)
-            => logger.LogException(null, exception, logLevel);
-
-        /// <summary>
-        /// Logs objects with it's public properties and fields as JSON object
-        /// </summary>
-        /// <param name="title">Title for entry</param>
-        /// <param name="item">Object that will be logged</param>
-        /// <param name="logLevel">Level for entry</param>
-        public static void Log(this JLogger logger, string title, object item, LogLevel logLevel = LogLevel.Info)
-            => logger.LogObject(title, item, logLevel);
-
-        /// <summary>
-        /// Logs objects with it's public properties and fields as JSON object
-        /// </summary>
-        /// <param name="item">Object that will be logged</param>
-        /// <param name="logLevel">Category for entry</param>
-        public static void Log(this JLogger logger, object item, LogLevel logLevel = LogLevel.Info)
-            => logger.LogObject(null, item, logLevel);
-
-        /// <summary>
-        /// Logs single-line text with 'Warning' level
-        /// </summary>
-        public static void Warning(this JLogger logger, string text)
-            => logger.LogText(text, null, LogLevel.Warning);
-
-        /// <summary>
-        /// Logs single-line text with 'Warning' level
-        /// </summary>
-        public static void Error(this JLogger logger, string text)
-            => logger.LogText(text, null, LogLevel.Critical);
-
-        /// <inheritdoc cref="JLogger.LogException(string, Exception, LogLevel)" />
-        public static void Exception(this JLogger logger, string title, Exception exception, LogLevel logLevel = LogLevel.Info)
-            => logger.LogException(title, exception, logLevel);
-
-        /// <inheritdoc cref="JLogger.LogObject(string, object, LogLevel)" />
-        public static void Object(this JLogger logger, string title, object data, LogLevel logLevel = LogLevel.Info)
-            => logger.LogObject(title, data, logLevel);
     }
 }
